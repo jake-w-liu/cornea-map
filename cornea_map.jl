b9beec866c07db18eb3beb580098f8bcd1381157
akjake616 <akjake616@gmail.com>
2024-11-05 18:05:58 +0800

    update main

--- cornea_map.jl
+++ cornea_map.jl
@@ -13,18 +13,22 @@ Base.@kwdef mutable struct Parameters
     Dc::Float64 = 30 # cornea diameter (nm)
     ri::Float64 = 1.504 # refractive index
     rho::Float64 = 0.00013147727272727272 # density
-    Sx::Float64 = 22000 # space lengh in x (nm)
-    Sy::Float64 = 30000 # space lengh in y (nm)
-    Sb::Float64 = 2000 # source boundary (nm)
+    # rho::Float64 = 0.000000013147727272727272 # density ## Test
+    Sx::Float64 = 20000 # space lengh in x (nm)
+    Sy::Float64 = 20000 # space lengh in y (nm)
+    St::Float64 = 1000 # source thickness (nm)
+    Sw::Float64 = 4000 # source width (nm)
+    Sp::Vector{Float64} = [500, 10000] # source center position (nm)
     Cb::Float64 = 100 # min distance from cylinder to boundary (nm)
     Nc::Int = round(Int, rho * Sx * Sy) # number of cornea fibers
     ds::Float64 = 10 # grid size (nm)
     r::Float64 = 60 # spacing between cornea fibers (nm)
     Nx::Int = round(Int, Sx / ds)
     Ny::Int = round(Int, Sy / ds)
-    xrange::Vector{Float64} = [1500, 2500] # plot range in x (nm)
-    yrange::Vector{Float64} = [0, 1000] # plot range in y (nm) 
+    xrange::Vector{Float64} = [0, 2000] # plot range in x (nm)
+    yrange::Vector{Float64} = [7000, 9000] # plot range in y (nm) 
     drift::Float64 = 20 # drift motion 
+    mn::Int = 100 # map number
 end
 
 mutable struct CorneaList
@@ -53,13 +57,30 @@ function find_neighbor(pos::Matrix{<:Real}, par::Parameters)
     return neighbor
 end
 
+function gen_position!(par::Parameters, pos::Vector, check::Bool = false)
+    check = false
+    while !check
+        pos[1] = (par.Sx - 2 * par.Cb) * rand() + par.Cb
+        pos[2] = (par.Sy - 2 * par.Cb) * rand() + par.Cb
+        if pos[1] > par.Sp[1] - par.St / 2 - par.Cb &&
+           pos[1] < par.Sp[1] + par.St / 2 + par.Cb &&
+           pos[2] > par.Sp[2] - par.Sw / 2 - par.Cb &&
+           pos[2] < par.Sp[2] + par.Sw / 2 + par.Cb
+            check = false
+        else
+            check = true
+        end
+    end
+
+    return nothing
+end
+
 function init_cornea(par::Parameters)
     cor_pos = zeros(par.Nc, 2)
-    pass = false
+    @all pass check = false
     @all rd pos = zeros(2)
     @inbounds @views for n = 1:par.Nc
-        pos[1] = (par.Sx - 2*par.Cb - par.Sb) * rand() + par.Cb + par.Sb
-        pos[2] = (par.Sy - 2*par.Cb) * rand() + par.Cb
+        gen_position!(par, pos, check)
         if n == 1
             cor_pos[n, :] .= pos
         else
@@ -67,8 +88,7 @@ function init_cornea(par::Parameters)
                 for nc = 1:n
                     rd .= cor_pos[nc, :] .- pos
                     if norm(rd) <= par.r
-                        pos[1] = (par.Sx - 2*par.Cb - par.Sb) * rand() + par.Cb + par.Sb
-                        pos[2] = (par.Sy - 2*par.Cb) * rand() + par.Cb
+                        gen_position!(par, pos, check)
                         break
                     end
                     if nc == n
@@ -90,26 +110,31 @@ end
 function update_cornea!(cl::CorneaList, par::Parameters)
     pass = false
     @all rd tmp = zeros(2)
-    N = size(cl.pos, 1)
-    @inbounds @views for n = 1:N
+    @inbounds @views for n in 1:par.Nc
         pass = false
         while !pass
             tmp[1] = cl.pos_ini[n, 1] + (rand() - 0.5) * par.drift * 2
             tmp[2] = cl.pos_ini[n, 2] + (rand() - 0.5) * par.drift * 2
-            if tmp[1] < par.Sb + par.Cb && tmp[1] > par.Sx - par.Cb && 
-                tmp[2] < par.Cb && tmp[2] > par.Sy - par.Cb 
+            if tmp[1] < par.Cb &&
+               tmp[1] > par.Sx - par.Cb &&
+               tmp[2] < par.Cb &&
+               tmp[2] > par.Sy - par.Cb
                 continue
             end
 
-            for nn = 1:length(cl.nb[n])
-                rd .= tmp .- cl.pos[cl.nb[n][nn], :]
-                if norm(rd) < par.r
-                    break
+            if !isempty(cl.nb[n]) 
+                for nn = 1:length(cl.nb[n])
+                    rd .= tmp .- cl.pos[cl.nb[n][nn], :]
+                    if norm(rd) < par.r
+                        break
+                    end
+                    if nn == length(cl.nb[n])
+                        cl.pos[n, :] .= tmp
+                        pass = true
+                    end
                 end
-                if nn == length(cl.nb[n])
-                    cl.pos[n, :] .= tmp
-                    pass = true
-                end
+            else
+                pass = true
             end
         end
     end
@@ -133,7 +158,7 @@ end
 
 function layout_include!(layout::PlotlyJS.Layout, cl::CorneaList, par::Parameters)
     layout.shapes = []
-    @inbounds @views for n in 1:par.Nc
+    @inbounds @views for n = 1:par.Nc
         if cl.pos[n, 1] > par.xrange[1] &&
            cl.pos[n, 1] < par.xrange[2] &&
            cl.pos[n, 2] > par.yrange[1] &&
@@ -159,6 +184,7 @@ end
 function update_plot!(plt::PlotlyJS.SyncPlot, cl::CorneaList, par::Parameters)
     layout_include!(plt.plot.layout, cl, par)
     react!(plt, plt.plot.data, plt.plot.layout)
+    return nothing
 end
 
 function plot_cornea(cl::CorneaList, par::Parameters)
@@ -167,14 +193,19 @@ function plot_cornea(cl::CorneaList, par::Parameters)
     return plt
 end
 
-function output_data(cl::CorneaList, par::Parameters, filename::String)
-    data = Vector{Vector{Float64}}(undef, par.Nc)
+function output_data(
+    cl::CorneaList,
+    par::Parameters,
+    filename::String,
+    data::Vector{Vector{Float64}},
+)
+    # data = Vector{Vector{Float64}}(undef, par.Nc)
     formatted_row = Vector{String}(undef, 4)
-    for n = 1:par.Nc
+    @views for n in 1:par.Nc
         data[n] = [cl.pos[n, 1] * 1e-9, cl.pos[n, 2] * 1e-9, par.Dc / 2 * 1e-9, par.ri]
     end
     open(filename, "w") do file
-        for row in data
+        @views for row in data
             formatted_row .= [@sprintf("%e", x) for x in row]
             println(file, join(formatted_row, ","))
         end
@@ -187,8 +218,10 @@ function output_grid(
     filename::String,
     bg_ind::Int,
     mtr_ind::Int,
+    grid::Matrix,
 )
-    grid = ones(Int, par.Nx, par.Ny) .* bg_ind
+    # grid = ones(Int, par.Nx, par.Ny) .* bg_ind
+    fill!(grid, bg_ind)
     x = range(0, (par.Nx - 1) * par.ds, par.Nx) .+ par.ds / 2
     y = range(0, (par.Ny - 1) * par.ds, par.Ny) .+ par.ds / 2
     @all indx indy = 0
@@ -215,44 +248,63 @@ end
 
 ## script
 
-par = Parameters()
-@time cl = init_cornea(par)
+function main()
+    par = Parameters()
+    @time cl = init_cornea(par)
 
-plt = plot_cornea(cl, par)
-display(plt)
+    plt = plot_cornea(cl, par)
+    display(plt)
 
-if !isdir("tmp")
-    mkdir("tmp")
-end
+    if !isdir("tmp")
+        mkdir("tmp")
+    end
 
-if !isdir("map")
-    mkdir("map")
-end
+    if !isdir("map")
+        mkdir("map")
+    end
 
-if !isdir("map_jld")
-    mkdir("map_jld")
-end
+    if !isdir("map_jld")
+        mkdir("map_jld")
+    end
 
-for nt = 1:100
-    @time update_cornea!(cl, par)
-    update_plot!(plt, cl, par)
+    file_name = "cornea_map_$(par.Sx/1000)by$(par.Sy/1000)_"
+    data = Vector{Vector{Float64}}(undef, par.Nc)
+    grid = ones(Int, par.Nx, par.Ny) .* 3
+    for nt = 1:par.mn
+        @time update_cornea!(cl, par)
+        update_plot!(plt, cl, par)
 
-    savefig(
-        plt,
-        "./tmp/snap_" * lpad(nt, 3, '0') * ".png";
-        height = 500,
-        width = round(Int, 500 / diff(par.xrange)[1] * diff(par.yrange)[1]),
+        savefig(
+            plt,
+            "./tmp/snap_" * lpad(nt, 3, '0') * ".png";
+            height = 500,
+            width = round(Int, 500 / diff(par.xrange)[1] * diff(par.yrange)[1]),
+        )
+
+        @time output_data(cl, par, "./map/" * file_name * lpad(nt, 3, '0') * ".dat", data)
+        @time output_grid(
+            cl,
+            par,
+            "./map_jld/" * file_name * lpad(nt, 3, '0') * ".jld",
+            3,
+            8,
+            grid,
+        )
+
+        println(nt)
+    end
+
+    framerate = 10
+    gifname = "output.gif"
+    FFMPEG.ffmpeg_exe(
+        `-framerate $(framerate) -f image2 -i ./tmp/snap_%03d.png -y $(gifname)`,
     )
-
-    @time output_data(cl, par, "./map/map_" * lpad(nt, 3, '0') * ".dat")
-    @time output_grid(cl, par, "./map_jld/map_" * lpad(nt, 3, '0') * ".jld", 3, 8)
-
-    println(nt)
+    return nothing
 end
 
-framerate = 10
-gifname = "output.gif"
-FFMPEG.ffmpeg_exe(`-framerate $(framerate) -f image2 -i ./tmp/snap_%03d.png -y $(gifname)`)
+main()
+
+
 
 # if isdir("tmp")
 #     rm("tmp"; recursive=true)
